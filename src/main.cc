2a3ccddc5f9a860a76a8293e9b2d41de72517096
Johanna Wald <johanna.wald@tum.de>
2018-08-17 14:38:58 +0200

    added header to main

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,12 @@
+/*******************************************************
+ * Copyright (c) 2018, Johanna Wald
+ * All rights reserved.
+ *
+ * This file is distributed under the GNU Lesser General Public License v3.0.
+ * The complete license agreement can be obtained at:
+ * http://www.gnu.org/licenses/lgpl-3.0.html
+ ********************************************************/
+
 #include "gui.h"
 
 int main(int argc, char** argv) {
